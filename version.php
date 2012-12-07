0862049cb5228c9be10ba4455cf7895f4abeb1ee
mpb <mpb@66863301-6fd9-46e3-8a26-2301b326d8a2>
2012-12-07 14:02:39 +0000

    Move version to seperate file, seems required for 2.x

---
+++ version.php
@@ -0,0 +1,5 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2012120700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->component = 'block_helpme'; // Full name of the plugin (used for diagnostics)
