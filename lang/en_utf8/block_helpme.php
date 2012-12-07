75ad9d477356386ebd5b5f8f1e8937e342fc617d
mpb <mpb@66863301-6fd9-46e3-8a26-2301b326d8a2>
2012-12-07 13:38:19 +0000

    Having elsa as the block name isn't logical - change to helpme.

---
+++ lang/en_utf8/block_helpme.php
@@ -0,0 +1,13 @@
+<?PHP
+//Engelsk tekst version til ELSA Moodle hjælp block
+
+$string['title'] = 'Help me!';
+$string['footertext'] = 'View all help subjects';
+$string['notregistered'] = 'This site is not registered in the ELSA support block.';
+$string['studentsee'] = 'Students will see:';
+$string['teachersee'] = 'Teachers will see:';
+
+//Denne bruges af elsa blocken til at tjekke hvilket sprog moodle efterspørger
+$string['elsalang'] = 'en';
+
+?>
